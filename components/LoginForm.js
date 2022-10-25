675c7db792d55282beade547d641723ba1299fd3
Sharmaine <sharmainejesmsantos@gmail.com>
2022-10-24 20:16:48 -0600

    add Login Form sample

---
+++ components/LoginForm.js
@@ -0,0 +1,11 @@
+import Button from "../ui/buttons/Button"
+
+function LoginForm(){
+    return (
+        <form action=''>
+            <Button label="Log In" variant="primary" />
+        </form>
+    )
+}
+
+export default LoginForm
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 
 import Button from "../ui/buttons/Button"
+import LoginForm from "../components/LoginForm"
+
+
 function HomePage() {   
     return  (
+
+      <>
       <header style={{textAlign:"center", marginTop:"10rem"}}>
           <Button label="basic button"/>
           <Button label="warning button"  variant="warning"/>
           <Button label="danger"  variant="danger" /> 
           <Button label="primary button"  variant="primary" /> 
       </header> 
-
+      <LoginForm></LoginForm>
+      </>
 
     )
 }
